951a42a6c8a60b171b4e9526f1e6289e27b49b22
Marco-A. Bauer <marcobauer@stackrocker.com>
2013-02-13 10:26:48 +0100

    updated

--- unittest/testcases/main.cpp
+++ unittest/testcases/main.cpp
@@ -3,10 +3,12 @@
 #include <cppunit/ui/text/TestRunner.h>
 
 #include "TestDataBuffer.h"
+#include "TestMessage.h"
 
 int main(int argc, char* argv[])
 {
 	CppUnit::TextUi::TestRunner runner;
 	runner.addTest( TestDataBuffer::suite() );
+	runner.addTest( TestMessage::suite() );
 	runner.run();
 }
